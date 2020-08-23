45b7624d96fa58a99fe0987d04af7dcbf3b97fbb
Ramo-94 <mohamedomar697@ymail.com>
2020-08-23 17:07:31 +0300

    Using express.js for server

--- util/hoster.js
+++ util/hoster.js
@@ -1,41 +1,16 @@
-const http = require('http')
-const { URL } = require('url');
-const fs = require('fs')
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+const path = require('path');
 
 module.exports = () => {
+
+
     //Image server to work on Discord's rich embed refresh issue
-    let server = http.createServer((req,res)=>{
 
+    app.use(express.static(path.join(__dirname, '../public')))
     
+    app.listen(port)
 
-        let pic
-        for (let i = 0; i < req.rawHeaders.length; i++) {
-            if (req.rawHeaders[i] === "pic")
-            pic = req.rawHeaders[i+1]
-            break;
-        }
-        
-        console.log("Log pic: ", pic)
-
-        let stream = fs.createReadStream(`./public/${pic}.png`)
-        stream.on('open',() => {
-            res.setHeader("Content-Type","image/png")
-            stream.pipe(res)
-
-
-            stream.on('end', ()=>{
-                stream.close()
-                res.end()
-            })
-
-            res.on('close', ()=>{
-                console.log("ended")
-            })
-        })
-
-    })
-
-    server.listen(process.env.PORT)
-
-    console.log("Server started listening on " + process.env.PORT)
+    console.log("Server started listening on " + port)
 }
