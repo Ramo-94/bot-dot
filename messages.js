98251c9f51fa58e7fcc09e834471068646fe8d71
Ramo-94 <mohamedomar697@ymail.com>
2020-08-29 02:33:53 +0300

    Added messages in one place

---
+++ messages.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    //Embed messages
+    DOWNLOAD_LINK: "Click to go to requested video link",
+
+    //Compression notice messages
+    NEEDS_COMPRESSION: "File too large, will try compression",
+
+    //Request status messages
+    REQUEST_ENQUEUED: "Request enqueued",
+    REQUEST_SEARCHING: "Searching..",
+    REQUEST_FOUND: "Found. Will try to download",
+    REQUEST_FULL: "Queue full, please wait",
+
+    //Errors
+    NAV_ERROR: "Network error",
+    COMPRESSION_LARGE: "File too large",
+
+}
