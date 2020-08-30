6e373379f40d5be2543e1c8667fb20e33c87e595
Ramo-94 <mohamedomar697@ymail.com>
2020-08-30 16:18:35 +0300

    Removed express from dep. Added two env auth roles

--- variables.js
+++ variables.js
@@ -2,5 +2,7 @@ module.exports = {
     TOKEN   : process.env.TOKEN,
     OWNER   : process.env.OWNER,
     PORT    : process.env.PORT,
-    APPLINK : process.env.APPLINK
+    APPLINK : process.env.APPLINK,
+    AUTH1   : process.env.AUTH1,
+    AUTH2   : process.env.AUTH2
 } 
