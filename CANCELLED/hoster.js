883e659ddaa947c62222770e2d7faacc3e6b6050
Ramo-94 <mohamedomar697@ymail.com>
2020-08-30 16:14:16 +0300

    Cancelled hoster util

---
+++ CANCELLED/hoster.js
@@ -0,0 +1,16 @@
+// const express = require('express')
+// const app = express()
+// const path = require('path')
+// const { PORT } = require('../variables')
+
+// module.exports = () => {
+
+
+//     //Image server to work on Discord's rich embed refresh issue
+
+//     app.use(express.static(path.join(__dirname, '../public')))
+    
+//     app.listen(PORT)
+
+//     console.log("Server started listening on " + PORT)
+// }
